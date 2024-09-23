7f5bc0b7ebf1693346da03e6c43d16b7fe1fe01e
Abdo Elbessa <zezoelbessa10@gmail.com>
2024-09-23 13:35:10 +0300

    Add project files.

---
+++ E-Commerce/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
